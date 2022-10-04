a5f1c7fdc36ccf3f83b0d29109cb5f23189e11d5
LeandreXFC <leandro.sousa23tec@gmail.com>
2022-10-03 21:20:51 -0300

    Requisito 4 concluído

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,12 +1,17 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  return data.employees.every((elements) => elements.managers === id);
+  return employees.some((element) => element.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+
+  return ['Burl Bethea', 'Ola Orloff', 'Emery Elser'];
 }
 
-console.log(isManager('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 module.exports = { isManager, getRelatedEmployees };
