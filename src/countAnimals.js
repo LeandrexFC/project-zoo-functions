8c3aa71dd9efbc281208b5b933f9438d0197adf1
LeandreXFC <leandro.sousa23tec@gmail.com>
2022-10-05 20:49:08 -0300

    Requisto 5 concluído

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -3,25 +3,23 @@ const data = require('../data/zoo_data');
 const { species } = data;
 
 function countAnimals(animal) {
-  const test = species.find(({ name }) => name === animal.specie);
-  const test2 = test.residents.length;
-  console.log(test2);
-  const test4 = test.residents.filter((elements) => elements.sex === 'female');
-  if (animal === undefined) {
-    return {
-      lions: 4,
-      tigers: 2,
-      bears: 3,
-      penguins: 4,
-      otters: 4,
-      frogs: 2,
-      snakes: 2,
-      elephants: 4,
-      giraffes: 6,
-    };
+  const obj = {};
+  if (!animal) {
+    species.forEach((elements) => { obj[elements.name] = elements.residents.length; });
+    return obj;
   }
-  return test4.length;
+  const { specie, sex } = animal;
+  if (specie !== undefined && sex !== undefined) {
+    const test = species.find(({ name }) => name === animal.specie);
+    const test4 = test.residents.filter(
+      (elements) => elements.sex === sex,
+    ).length;
+    return test4;
+  }
+  if (specie !== undefined) {
+    const test = species.find(({ name }) => name === animal.specie);
+    const test2 = test.residents.length;
+    return test2;
+  }
 }
-
-console.log(countAnimals());
 module.exports = countAnimals;
