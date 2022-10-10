468987b9779a46feca843d103c1609a87f85a59c
LeandreXFC <leandro.sousa23tec@gmail.com>
2022-10-10 19:18:39 -0300

    Ultimo requisito

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,34 @@
 const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
-function getEmployeesCoverage(names, ids) {
+const filterElements = (obj) => employees.find((elements) => elements.firstName === obj.name
+|| elements.lastName === obj.name || elements.id === obj.id);
 
+const findSpecies = (specie) => specie.responsibleFor.map((id) =>
+  species.find((elements) => (elements.id === id)));
+
+const getAllPeople = () => employees.map((elements) => ({
+  id: elements.id,
+  fullName: `${elements.firstName} ${elements.lastName}`,
+  species: findSpecies(elements).map((element) => element.name),
+  locations: findSpecies(elements).map((element) => element.location),
+}));
+
+function getEmployeesCoverage(object) {
+  if (object === undefined) {
+    return getAllPeople();
+  }
+  if (filterElements(object) === undefined) {
+    throw new Error('Informações inválidas');
+  }
+  const finalResults = {
+    id: filterElements(object).id,
+    fullName: `${filterElements(object).firstName} ${filterElements(object).lastName}`,
+    species: findSpecies(filterElements(object)).map((elements) => elements.name),
+    locations: findSpecies(filterElements(object)).map((elements) => elements.location),
+  };
+
+  return finalResults;
 }
 
 console.log(getEmployeesCoverage({ name: 'Spry' }));
